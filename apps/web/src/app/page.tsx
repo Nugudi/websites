1577fd0b8483e9a7e5b563a8d457cf0fba92c80c
김용민 <dydals3440@gmail.com>
2025-06-14 21:32:43 +0900

    feat(web): Web파일 추가

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='text-2xl font-bold text-green-500'>
+        너의 구로 디지털 단지
+      </h1>
+    </div>
+  );
+}
