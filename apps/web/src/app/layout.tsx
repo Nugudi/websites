0a9db25290ef4aeb1611b79bcb6dab924951d79a
김용민 <dydals3440@gmail.com>
2025-06-14 21:49:54 +0900

    feat(root): biome linter, turbo formatter 설정을 추가

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: '너구디',
-  description: '너의 구로 디지털단지',
+  title: "너구디",
+  description: "너의 구로 디지털단지",
 };
 
 export default function RootLayout({
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='ko'>
+    <html lang="ko">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
     <div>
-      <h1 className='text-2xl font-bold text-green-500'>
+      <h1 className="font-bold text-2xl text-green-500">
         너의 구로 디지털 단지
       </h1>
     </div>
