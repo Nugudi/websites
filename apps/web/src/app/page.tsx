571590c710c3231e4b4cfcdb950d5394bd458395
김용민 <dydals3440@gmail.com>
2025-06-16 08:36:43 +0900

    feat(ui): 스토리북 구현

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,13 @@
+import { NugudiButton } from "@nugudi/nugudi-ui/nugudi-button";
+
 export default function Home() {
   return (
     <div>
       <h1 className="font-bold text-2xl text-green-500">
         너의 구로 디지털 단지
       </h1>
+      <NugudiButton>123</NugudiButton>
+      {/* <NugudiButton>너구디구디</NugudiButton> */}
     </div>
   );
 }
