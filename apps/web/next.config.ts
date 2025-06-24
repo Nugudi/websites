9db1348e7e89d11cd4c5ae3cc048429a28f0259c
김용민 <dydals3440@gmail.com>
2025-06-24 17:07:14 +0900

    feat(web): Sentry 서버/클라이언트 사이드 관련 설정 추가

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,28 @@
+import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const plugins = [
+  (config: NextConfig) =>
+    withSentryConfig(config, {
+      org: "nugudi",
+      project: "nugudi-web",
+      bundleSizeOptimizations: {
+        excludeDebugStatements: true,
+        excludeReplayShadowDom: true,
+        excludeReplayIframe: true,
+        excludeReplayWorker: true,
+      },
+      disableLogger: true,
+      silent: !process.env.CI,
+      sourcemaps: {
+        deleteSourcemapsAfterUpload: true,
+      },
+      telemetry: false,
+      tunnelRoute: "/monitoring",
+      widenClientFileUpload: true,
+    }),
+];
 
-export default nextConfig;
+export default plugins.reduce((acc, plugin) => plugin(acc), {
+  reactStrictMode: true,
+} as NextConfig);
---
+++ apps/web/sentry.edge.config.ts
@@ -0,0 +1,6 @@
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://f7bee84446ba1c3f4a3e1c1011663dbc@o4509552570990592.ingest.us.sentry.io/4509552579313664",
+  debug: false,
+});
