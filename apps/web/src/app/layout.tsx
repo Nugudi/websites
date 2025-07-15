6a624d95f9a2a5d82a5c3353abe932c4a47649d6
김용민 <dydals3440@gmail.com>
2025-07-15 20:53:28 +0900

    feat(ui): figma 폰트 관련 디자인 시스템 적용, tailwind css 플러그인 동작 방식 변경

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,16 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./app.css";
+import localFont from "next/font/local";
 import Providers from "@/shared/providers";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const Pretendard = localFont({
+  src: "../../node_modules/pretendard/dist/web/variable/woff2/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  adjustFontFallback: false,
+  variable: "--font-pretendard",
 });
 
 export const metadata: Metadata = {
@@ -37,9 +35,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko" suppressHydrationWarning={true}>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={Pretendard.variable}>
         <Providers>{children}</Providers>
       </body>
     </html>
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,7 +3,8 @@ import { Button } from "@nugudi/nugudi-ui/button";
 export default function Home() {
   return (
     <div>
-      <h1 className="font-bold text-2xl text-green-500">
+      <h1 className="text-title1-semi28">너의 구로 디지털 단지</h1>
+      <h1 className="font-bold text-green-500 text-title1-semi28">
         너의 구로 디지털 단지
       </h1>
       <Button>너구디 버튼</Button>
