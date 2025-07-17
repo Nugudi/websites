29db9e7d0a5345178b1d326225ed05431a2e57ff
hijjoy <hijjoy@naver.com>
2025-07-17 21:10:14 +0900

    fix(ui): color 스토리북 수정
    color 스토리북 디자인 수정
    
    12

--- packages/ui/src/foundations/color.stories.tsx
+++ packages/ui/src/foundations/color.stories.tsx
@@ -15,17 +15,6 @@ const colors = [
   "black",
 ];
 
-const bgMap: Record<string, string> = {
-  white: "bg-zinc-800",
-  "zinc-50": "bg-zinc-700",
-  "zinc-100": "bg-zinc-700",
-  "zinc-200": "bg-zinc-700",
-  black: "bg-zinc-100",
-  "main-500": "bg-white",
-  "main-800": "bg-zinc-50",
-  error: "bg-white",
-};
-
 export default {
   title: "Foundations/Color",
   parameters: {
@@ -34,20 +23,17 @@ export default {
 };
 
 export const Color = () => (
-  <div className="grid grid-cols-3 gap-4 p-8 md:grid-cols-4 lg:grid-cols-5">
+  <div className="grid grid-cols-3 gap-10 p-8 md:grid-cols-4 lg:grid-cols-5">
     {colors.map((color) => {
-      const bg =
-        bgMap[color] ||
-        (color.startsWith("zinc-") && parseInt(color.split("-")[1], 10) > 400
-          ? "bg-zinc-100"
-          : "bg-white");
       return (
         <div
           key={color}
-          className={`flex items-center gap-4 ${bg} rounded-md border p-3`}
+          className={`flex flex-col gap-2 rounded-md border border-zinc-50 p-2`}
         >
-          <span className="w-20 font-mono text-main-500 text-xs">{color}</span>
-          <span className={`b1 text-${color} font-semibold`}>Aa</span>
+          <div
+            className={`bg-${color} h-20 w-full rounded-md border border-zinc-50`}
+          />
+          <span className="w-20 font-mono text-xs text-zinc-700">{color}</span>
         </div>
       );
     })}
