7157fde1d62264af31fc44c4ad840fc0dbb0ee3b
김용민 <dydals3440@gmail.com>
2025-08-12 21:58:52 +0900

    [NUGUDI-67] refactor(web): Schema 유효성 검사 서비스 성격에 맞게 변경

--- apps/web/src/domains/auth/sign-up/schemas/sign-up-schema.ts
+++ apps/web/src/domains/auth/sign-up/schemas/sign-up-schema.ts
@@ -3,9 +3,22 @@ import { z } from "zod";
 export const signUpSchema = z.object({
   email: z.email("올바른 이메일 주소를 입력해주세요"),
   code: z.string().min(6, "올바른 인증 코드를 입력해주세요"),
-  password: z.string().min(8, "최소 8자 이상 입력해주세요"),
-  passwordConfirm: z.string().min(8, "최소 8자 이상 입력해주세요"),
-  nickname: z.string().min(2, "최소 2자 이상 입력해주세요"),
+  password: z
+    .string()
+    .min(8, "최소 8자 이상 입력해주세요")
+    .max(20, "최대 20자 이하로 입력해주세요")
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]+$/,
+      "영문, 숫자, 특수문자, 대문자를 각각 1개 이상 포함해주세요",
+    ),
+  passwordConfirm: z
+    .string()
+    .min(8, "최소 8자 이상 입력해주세요")
+    .max(20, "최대 20자 이하로 입력해주세요"),
+  nickname: z
+    .string()
+    .min(2, "최소 2자 이상 입력해주세요")
+    .max(12, "최대 12자 이하로 입력해주세요"),
 });
 
 export const signUpEmailSchema = signUpSchema.pick({
