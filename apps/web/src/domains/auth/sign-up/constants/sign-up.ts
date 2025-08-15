6aae9d0fec921faf63904bd7ab6e708a03cfda49
김용민 <dydals3440@gmail.com>
2025-08-15 22:59:00 +0900

    [NUGUDI-86] feat(web): 회원가입 단계에 따라 StepIndicator 적용

--- apps/web/src/domains/auth/sign-up/constants/sign-up.ts
+++ apps/web/src/domains/auth/sign-up/constants/sign-up.ts
@@ -1,2 +1,2 @@
-export const TOTAL_SIGN_UP_STEPS = 3;
+export const TOTAL_SIGN_UP_STEPS = 5;
 export const SIGN_UP_STORE_KEY = "sign-up-store";
--- apps/web/src/domains/auth/sign-up/ui/components/sign-up-form/index.tsx
+++ apps/web/src/domains/auth/sign-up/ui/components/sign-up-form/index.tsx
@@ -1,7 +1,11 @@
 "use client";
 
+import { Flex } from "@nugudi/react-components-layout";
+import { StepIndicator } from "@nugudi/react-components-step-indicator";
 import { useEffect } from "react";
+import { TOTAL_SIGN_UP_STEPS } from "@/src/domains/auth/sign-up/constants/sign-up";
 import { useSignUpStore } from "@/src/domains/auth/sign-up/stores/use-sign-up-store";
+import * as styles from "./index.css";
 import AgreementForm from "./steps/agreement-form";
 import EmailForm from "./steps/email-form";
 import EmailVerificationCodeForm from "./steps/email-verification-code-form";
@@ -17,13 +21,19 @@ const SignUpForm = () => {
   }, [useSignUpStore.persist?.hasHydrated]);
 
   return (
-    <>
+    <Flex
+      direction="column"
+      align="center"
+      justify="center"
+      className={styles.container}
+    >
+      <StepIndicator currentStep={step} totalSteps={TOTAL_SIGN_UP_STEPS} />
       {step === 1 && <EmailForm />}
       {step === 2 && <EmailVerificationCodeForm />}
       {step === 3 && <PasswordForm />}
       {step === 4 && <NicknameForm />}
       {step === 5 && <AgreementForm />}
-    </>
+    </Flex>
   );
 };
 
