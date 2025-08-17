f5e5087747cd10010a6859221045ffb43bb7342f
김용민 <dydals3440@gmail.com>
2025-08-17 19:45:48 +0900

    [NUGUDI-95] feat(web): EmailVerficationCodeForm UI 및 기능 구현

---
+++ apps/web/src/domains/auth/password-forgot/ui/components/password-forgot-form/steps/email-verification-code-form/index.css.ts
@@ -0,0 +1,17 @@
+import { style } from "@vanilla-extract/css";
+
+export const form = style({
+  display: "flex",
+  flexDirection: "column",
+  width: "100%",
+  flex: 1,
+});
+
+export const titleContainer = style({
+  marginTop: "32px",
+});
+
+export const inputOtpContainer = style({
+  flex: 1,
+  marginTop: "32px",
+});
