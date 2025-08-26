6b73e2971d7b87349b8487e573d548b0d4839734
김용민 <dydals3440@gmail.com>
2025-08-26 17:48:26 +0900

    [NUGUDI-105] chore(web): 미사용 import 정리

--- apps/web/app/(auth)/benefits/page.tsx
+++ apps/web/app/(auth)/benefits/page.tsx
@@ -1,8 +1,13 @@
+import { BottomSheetContainer } from "@/src/shared/ui/components/bottom-sheet/bottom-sheet-container";
+
 const BenefitsPage = () => {
   return (
     <div>
-      BenefitsPage는 tab 바 이동때문에 생성하였습니다. 추후 경로 이동이 있을 수
-      있습니다 !
+      <h1>
+        BenefitsPage는 tab 바 이동때문에 생성하였습니다. 추후 경로 이동이 있을
+        수 있습니다 !
+      </h1>
+      <BottomSheetContainer />
     </div>
   );
 };
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,3 @@
-import { Flex } from "@nugudi/react-components-layout";
-import { MenuCard } from "@nugudi/react-components-menu-card";
-import Link from "next/link";
-import TabBar from "@/src/shared/ui/components/tab-bar";
-import * as styles from "./page.css";
-
 import HomeView from "@/src/domains/restaurant/ui/views/home-view";
 
 const HomePage = () => {
