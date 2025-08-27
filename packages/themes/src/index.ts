00bd27bdd88f4d0be43418612d27d70599a54419
hijjoy <hijjoy@naver.com>
2025-08-27 19:15:15 +0900

    [NUGUDI-112] refactor(themes): ColorShade 타입 export 방식으로 수정
    - ColorShade 타입을 themes 패키지에서 직접 export
    - COLOR_SHADES 상수와 ColorShade 타입 분리로 타입 안정성 향상

--- packages/themes/src/index.ts
+++ packages/themes/src/index.ts
@@ -1,3 +1,3 @@
 export * as classes from "./classes";
-export { COLOR_SHADES } from "./constants/color";
+export { COLOR_SHADES, type ColorShade } from "./constants/color";
 export * as vars from "./variables";
