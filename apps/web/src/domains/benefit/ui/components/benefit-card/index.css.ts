ca12211019ffde8ac1840a37d202630df82a094a
김용민 <dydals3440@gmail.com>
2025-08-30 17:53:47 +0900

    [NUGUDI-114] feat(web): BenefitCard 컴포넌트 디자인 구현

--- apps/web/src/domains/benefit/ui/components/benefit-card/index.css.ts
+++ apps/web/src/domains/benefit/ui/components/benefit-card/index.css.ts
@@ -2,19 +2,36 @@ import { vars } from "@nugudi/themes";
 import { style } from "@vanilla-extract/css";
 
 export const card = style({
-  width: "149px",
   height: "152px",
   backgroundColor: vars.colors.$scale.whiteAlpha[100],
-  borderRadius: vars.box.radii.lg,
+  border: `1px solid ${vars.colors.$scale.zinc[50]}`,
+  borderRadius: "10px",
   padding: vars.box.spacing[16],
-  boxShadow: vars.box.shadows.sm,
   display: "flex",
-  flexDirection: "column",
-  justifyContent: "space-between",
+  position: "relative",
   cursor: "pointer",
   transition: "all 0.2s ease",
   ":hover": {
-    boxShadow: vars.box.shadows.md,
     transform: "translateY(-2px)",
+    boxShadow: vars.box.shadows.sm,
   },
+});
+
+export const contentWrapper = style({
+  display: "flex",
+  flexDirection: "column",
+  position: "absolute",
+  textAlign: "left",
+  top: 20,
+  left: 20,
+  gap: vars.box.spacing[4],
+});
+
+export const iconWrapper = style({
+  position: "absolute",
+  bottom: vars.box.spacing[8],
+  right: vars.box.spacing[8],
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 });
--- apps/web/src/domains/benefit/ui/components/benefit-card/index.tsx
+++ apps/web/src/domains/benefit/ui/components/benefit-card/index.tsx
@@ -1,5 +1,27 @@
+import { Box, GridItem } from "@nugudi/react-components-layout";
+import type { ReactNode } from "react";
 import * as styles from "./index.css";
 
-export const BenefitCard = () => {
-  return <div className={styles.card}>BenefitCard</div>;
+interface BenefitCardProps {
+  title: ReactNode;
+  description: ReactNode;
+  icon?: ReactNode;
+  onClick?: () => void;
+}
+
+export const BenefitCard = ({
+  title,
+  description,
+  icon,
+  onClick,
+}: BenefitCardProps) => {
+  return (
+    <GridItem className={styles.card} onClick={onClick}>
+      <Box className={styles.contentWrapper}>
+        {title}
+        {description}
+      </Box>
+      {icon && <Box className={styles.iconWrapper}>{icon}</Box>}
+    </GridItem>
+  );
 };
