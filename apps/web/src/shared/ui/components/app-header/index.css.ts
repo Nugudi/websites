18cb4bc7d10e8cdb85cdfcf52e040e912ada3b16
김용민 <dydals3440@gmail.com>
2025-08-30 18:27:59 +0900

    [NUGUDI-114] feat(web): AppHeader 컴포넌트 구현

---
+++ apps/web/src/shared/ui/components/app-header/index.css.ts
@@ -0,0 +1,7 @@
+import { vars } from "@nugudi/themes";
+import { style } from "@vanilla-extract/css";
+
+export const container = style({
+  width: "100%",
+  backgroundColor: vars.colors.$static.light.color.white,
+});
