08878439f228443fbe161d8db7737290e00de7e4
hyewon <hijjoy04@gmail.com>
2025-09-04 22:17:44 +0900

    [NUGUDI-127] style(react): Comment 컴포넌트 스타일 변수로 변경
    - 기존 px로 정의되어있던 스타일 스타일 변수로 변경

--- packages/react/components/comment/src/style.css.ts
+++ packages/react/components/comment/src/style.css.ts
@@ -3,8 +3,8 @@ import { style } from "@vanilla-extract/css";
 
 export const commentContainer = style({
   display: "flex",
-  gap: "12px",
-  padding: "16px 0",
+  gap: vars.box.spacing[3],
+  padding: `${vars.box.spacing[4]} 0`,
   borderBottom: `1px solid ${vars.colors.$scale.zinc[100]}`,
 
   selectors: {
@@ -15,14 +15,14 @@ export const commentContainer = style({
 });
 
 export const reply = style({
-  paddingLeft: "48px",
+  paddingLeft: vars.box.spacing[12],
   position: "relative",
 });
 
 export const replyIconWrapper = style({
   color: vars.colors.$scale.zinc[500],
   position: "absolute",
-  left: "20px",
+  left: vars.box.spacing[5],
   top: "18px",
   display: "flex",
   alignItems: "center",
@@ -36,19 +36,19 @@ export const commentContent = style({
 
 export const header = style({
   display: "flex",
-  gap: "12px",
-  marginBottom: "12px",
+  gap: vars.box.spacing[3],
+  marginBottom: vars.box.spacing[3],
 });
 
 export const avatarSection = style({
   flexShrink: 0,
-  width: "40px",
-  height: "40px",
+  width: vars.box.spacing[10],
+  height: vars.box.spacing[10],
 });
 
 export const defaultAvatar = style({
-  width: "40px",
-  height: "40px",
+  width: vars.box.spacing[10],
+  height: vars.box.spacing[10],
   borderRadius: "50%",
   backgroundColor: vars.colors.$scale.zinc[300],
 });
@@ -64,7 +64,7 @@ export const headerInfo = style({
 export const userInfo = style({
   display: "flex",
   alignItems: "center",
-  gap: "8px",
+  gap: vars.box.spacing[2],
   flexShrink: 1,
   minWidth: 0,
 });
@@ -73,7 +73,7 @@ export const level = style({
   ...classes.typography.emphasis.e1,
   color: vars.colors.$scale.zinc[500],
   backgroundColor: vars.colors.$scale.zinc[100],
-  padding: "2px 8px",
+  padding: `2px ${vars.box.spacing[2]}`,
   borderRadius: vars.box.radii.sm,
   flexShrink: 0,
 });
@@ -90,11 +90,11 @@ export const timeAgo = style({
   ...classes.typography.emphasis.e2,
   color: vars.colors.$scale.zinc[400],
   flexShrink: 0,
-  marginLeft: "8px",
+  marginLeft: vars.box.spacing[2],
 });
 
 export const body = style({
-  paddingLeft: "52px",
+  paddingLeft: vars.box.spacing[14],
 });
 
 export const content = style({
@@ -105,7 +105,7 @@ export const content = style({
 });
 
 export const replies = style({
-  marginTop: "16px",
+  marginTop: vars.box.spacing[4],
   borderTop: `1px solid ${vars.colors.$scale.zinc[100]}`,
-  paddingTop: "16px",
+  paddingTop: vars.box.spacing[4],
 });
