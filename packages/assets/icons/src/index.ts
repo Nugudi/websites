c87c5fe30f54a86aed543a5e403c31b2c0594e9d
hyewon <hijjoy04@gmail.com>
2025-09-06 00:54:24 +0900

    [NUGUDI-128] feat(assets): UserFill 아이콘 추가
    - avatar 기본 아이콘을 위한 svg 추가

--- packages/assets/icons/src/index.ts
+++ packages/assets/icons/src/index.ts
@@ -40,4 +40,5 @@ export { default as SoupIcon } from "./svg/soup.svg?react";
 export { default as SubDishIcon } from "./svg/sub-dish.svg?react";
 export { default as SuccessIcon } from "./svg/success.svg?react";
 export { default as TeaIcon } from "./svg/tea.svg?react";
+export { default as UserFillIcon } from "./svg/user_fill.svg?react";
 export * from "./types/icon";
