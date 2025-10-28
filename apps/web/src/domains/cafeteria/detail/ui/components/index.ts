41e7e8042609fc1f3792c490911fa5d63e182f3b
hyewon <hijjoy04@gmail.com>
2025-10-28 21:36:01 +0900

    [NUGUDI-191] chore(web): 삭제된 ui 관련 export문 삭제
    - 삭제된 cafeteria-review-tab 관련 컴포넌트들 export 삭제

--- apps/web/src/domains/cafeteria/detail/ui/components/index.ts
+++ apps/web/src/domains/cafeteria/detail/ui/components/index.ts
@@ -1,5 +1,4 @@
 export * from "./cafeteria-info-card";
 export * from "./cafeteria-info-tab";
 export * from "./cafeteria-menu-bottom-sheet";
-export * from "./cafeteria-review-tab";
-export * from "./cafeteria-review-tab-controls";
+export * from "./cafeteria-menu-tab";
